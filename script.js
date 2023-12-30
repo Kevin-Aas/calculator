9a2a4a704d6aadc9d9e1a7646c36843aa7ad9807
Kevin <kevinalexander@live.no>
2023-12-30 02:36:35 +0100

    Make operate with intital pair of numbers

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let num1;
 let num2;
 let operator;
 
-let display_val;
+let display_val = 0;
 
 // Basic math operations:
 function add(a, b) {
@@ -22,20 +22,45 @@ function divide(a, b) {
 // Operation function:
 function operate(num1, num2, operator) {
     switch (operator) {
-        case '+': add(num1, num2);
-        case '-': subtract(num1, num2);
-        case '*': multiply(num1, num2);
-        case '/': divide(num1, num2);
+        case '+': return add(num1, num2);
+        case '-': return subtract(num1, num2);
+        case '*': return multiply(num1, num2);
+        case '/': return divide(num1, num2);
     }
 };
 
-//
+// Add listeners to the number buttons
 let numbers = document.querySelectorAll(".num");
 numbers.forEach(function (btn) {
     btn.addEventListener('click', (e) => {
-        display_val = e.target.textContent;
+        if (display_val === 0) {
+            display_val = e.target.textContent;
+        }
+        else {
+            display_val = display_val + e.target.textContent;
+        }
         updateDisplay(display_val);
     });
+});
+
+// Add listeners to the operation buttons
+let operations = document.querySelectorAll(".operation");
+operations.forEach(function (btn) {
+    btn.addEventListener('click', (e) => {
+        num1 = display_val;
+        operator = e.target.textContent;
+        display_val = display_val + operator;
+        updateDisplay(display_val);
+    });
+});
+
+// Add equals listener
+let equals = document.querySelector("#equals");
+equals.addEventListener('click', (e) => {
+    let values = display_val.split('+').join(',').split('-').join(',').split('*').join(',').split('/').join(',').split(',');
+    num2 = values[1];
+    display_val = operate(parseInt(num1), parseInt(num2), operator);
+    updateDisplay(display_val);
 });
 
 function updateDisplay (value) {
