b1289e6dbe4fecb14e97d847ec04ffee0f6d47f4
Kevin <kevinalexander@live.no>
2023-12-30 01:36:14 +0100

    Make numbers appear after pressing btns

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ let num1;
 let num2;
 let operator;
 
+let display_val;
+
 // Basic math operations:
 function add(a, b) {
     return a+b;
@@ -25,4 +27,18 @@ function operate(num1, num2, operator) {
         case '*': multiply(num1, num2);
         case '/': divide(num1, num2);
     }
+};
+
+//
+let numbers = document.querySelectorAll(".num");
+numbers.forEach(function (btn) {
+    btn.addEventListener('click', (e) => {
+        display_val = e.target.textContent;
+        updateDisplay(display_val);
+    });
+});
+
+function updateDisplay (value) {
+    let display = document.querySelector(".display");
+    display.textContent = value;
 };
